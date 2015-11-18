58b98f299718e2ae341daf1620884512c5045ceb
pjdufour <pjdufour.dev@gmail.com>
2015-11-17 23:57:39 -0500

    0.0.2 fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tilejet-util',
-    version='0.0.1',
+    version='0.0.2',
     install_requires=[],
     author='TileJet Developers',
     author_email='tilejet.dev@gmail.com',
